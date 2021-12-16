db91d881722178790b8fc344f2f63f6317cd94b5
Natanael Lima <taelima1997@gmail.com>
2021-12-16 00:19:00 -0300

    add final implementation list animated to index

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
-import { Entypo, Feather } from '@expo/vector-icons';
-import faker from 'faker';
-import * as React from 'react';
-import { Dimensions, FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { Entypo, Feather } from "@expo/vector-icons";
+import { MotiView } from "@motify/components";
+import faker from "faker";
+import * as React from "react";
+import {
+  Dimensions,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-const { width, height } = Dimensions.get('screen');
+const { width, height } = Dimensions.get("screen");
 
 faker.seed(10);
 
@@ -19,124 +26,190 @@ const _colors = {
 const _spacing = 10;
 
 export default function UberEats() {
+  const ref = React.useRef<FlatList>(null);
+
+  const [index, setIndex] = React.useState(0);
+  const [viewPosition, setViewPosition] = React.useState(0);
+
+  React.useEffect(() => {
+    ref.current?.scrollToIndex({
+      index,
+      animated: true,
+      viewPosition,
+      viewOffset: viewPosition === 0.5 || viewPosition === 1 ? 0 : _spacing,
+    });
+  }, [index, viewPosition]);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <FlatList
+        ref={ref}
         style={{ flexGrow: 0 }}
         data={data}
         keyExtractor={(item) => item.key}
         contentContainerStyle={{ paddingLeft: _spacing }}
         showsHorizontalScrollIndicator={false}
+        initialScrollIndex={index}
         horizontal
         renderItem={({ item, index: fIndex }) => {
           return (
-            <TouchableOpacity onPress={() => {}}>
-              <View
+            <TouchableOpacity
+              onPress={() => {
+                setIndex(fIndex);
+              }}
+            >
+              <MotiView
+                animate={{
+                  backgroundColor:
+                    fIndex === index ? _colors.active : _colors.inactive,
+                    opacity : fIndex === index ? 1 : 0.6
+                }}
+                transition={{
+                    duration : 500
+                }}
                 style={{
                   marginRight: _spacing,
                   padding: _spacing,
                   borderWidth: 2,
                   borderColor: _colors.active,
                   borderRadius: 12,
-                  backgroundColor: _colors.inactive,
-                }}>
-                <Text style={{ color: '#36303F', fontWeight: '700' }}>
+                }}
+              >
+                <Text style={{ color: "#36303F", fontWeight: "700" }}>
                   {item.job}
                 </Text>
-              </View>
+              </MotiView>
             </TouchableOpacity>
           );
         }}
       />
       <View
         style={{
-          alignItems: 'center',
-          flexDirection: 'row',
+          alignItems: "center",
+          flexDirection: "row",
           marginTop: _spacing * 10,
-        }}>
-        <View style={{ alignItems: 'center' }}>
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
           <Text
             style={{
-              color: '#36303F',
-              fontWeight: '700',
+              color: "#36303F",
+              fontWeight: "700",
               marginBottom: _spacing,
-            }}>
+            }}
+          >
             Scroll position
           </Text>
           <View
             style={{
-              flexDirection: 'row',
+              flexDirection: "row",
               width: width / 2,
-              justifyContent: 'center',
-            }}>
-            <TouchableOpacity onPress={() => {}}>
+              justifyContent: "center",
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                setViewPosition(0);
+              }}
+            >
               <View
                 style={{
                   padding: _spacing,
-                  backgroundColor: '#FCD259',
+                  backgroundColor: "#FCD259",
                   borderRadius: _spacing,
                   marginRight: _spacing,
-                }}>
-                <Entypo name='align-left' size={24} color='#36303F' />
+                }}
+              >
+                <Entypo name="align-left" size={24} color="#36303F" />
               </View>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => {}}>
+            <TouchableOpacity
+              onPress={() => {
+                setViewPosition(0.5);
+              }}
+            >
               <View
                 style={{
                   padding: _spacing,
-                  backgroundColor: '#FCD259',
+                  backgroundColor: "#FCD259",
                   borderRadius: _spacing,
                   marginRight: _spacing,
-                }}>
+                }}
+              >
                 <Entypo
-                  name='align-horizontal-middle'
+                  name="align-horizontal-middle"
                   size={24}
-                  color='#36303F'
+                  color="#36303F"
                 />
               </View>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => {}}>
+            <TouchableOpacity
+              onPress={() => {
+                setViewPosition(1);
+              }}
+            >
               <View
                 style={{
                   padding: _spacing,
-                  backgroundColor: '#FCD259',
+                  backgroundColor: "#FCD259",
                   borderRadius: _spacing,
-                }}>
-                <Entypo name='align-right' size={24} color='#36303F' />
+                }}
+              >
+                <Entypo name="align-right" size={24} color="#36303F" />
               </View>
             </TouchableOpacity>
           </View>
         </View>
-        <View style={{ alignItems: 'center' }}>
+        <View style={{ alignItems: "center" }}>
           <Text
-            style={{ color: '#36303F', fontWeight: '700', marginBottom: 10 }}>
+            style={{ color: "#36303F", fontWeight: "700", marginBottom: 10 }}
+          >
             Navigation
           </Text>
           <View
             style={{
-              flexDirection: 'row',
+              flexDirection: "row",
               width: width / 2,
-              justifyContent: 'center',
-            }}>
-            <TouchableOpacity onPress={() => {}}>
+              justifyContent: "center",
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                if (index === 0) {
+                  return;
+                }
+
+                setIndex(index - 1);
+              }}
+            >
               <View
                 style={{
                   padding: _spacing,
-                  backgroundColor: '#FCD259',
+                  backgroundColor: "#FCD259",
                   borderRadius: _spacing,
                   marginRight: _spacing,
-                }}>
-                <Feather name='arrow-left' size={24} color='#36303F' />
+                }}
+              >
+                <Feather name="arrow-left" size={24} color="#36303F" />
               </View>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => {}}>
+            <TouchableOpacity
+              onPress={() => {
+                if (index === data.length - 1) {
+                  return;
+                }
+
+                setIndex(index + 1);
+              }}
+            >
               <View
                 style={{
                   padding: _spacing,
-                  backgroundColor: '#FCD259',
+                  backgroundColor: "#FCD259",
                   borderRadius: _spacing,
-                }}>
-                <Feather name='arrow-right' size={24} color='#36303F' />
+                }}
+              >
+                <Feather name="arrow-right" size={24} color="#36303F" />
               </View>
             </TouchableOpacity>
           </View>
